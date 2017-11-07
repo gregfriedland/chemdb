430489b43e4f3c4a4a98bb0d84bc1df4be0ca97d
Greg Friedland <greg.friedland@gmail.com>
2017-11-06 16:10:37 -0800

    add node server docker file

--- web/server.js
+++ web/server.js
@@ -3,4 +3,8 @@ const app = express()
 
 app.use(express.static('public'))
 
+app.get('/query', function (req, res) {
+  res.send('Hello World!');
+});
+
 app.listen(8080, () => console.log('Example app listening on port 8080!'))
